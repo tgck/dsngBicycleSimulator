c3ce2b26f968e08a3b2add0f2c3fd75c7f2f7be6
tgck <taniguchiko@gmail.com>
2014-11-16 02:22:59 +0900

    キーイベントをファイル化

--- xcode/src/ofApp.cpp
+++ xcode/src/ofApp.cpp
@@ -33,7 +33,7 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
-    // センサを使う場合は、OSC受信メッセージからbikeモデルを更新
+    // センサを使う場合は、OSC受信メッセージからbikeモデルを更新する
     if (bUseSensor) {
         updateReceiver();
     }
@@ -42,7 +42,7 @@ void ofApp::update(){
     if (bAnimate) bike.update();
     
     // 通信
-    updateSender(); // フラグの考慮は先方でやってくれる
+    updateSender(); // フラグの考慮は呼び出し先でやってくれる
     
 }
 
@@ -79,77 +79,3 @@ void ofApp::draw(){
 }
 
 
-//--------------------------------------------------------------
-//
-void ofApp::keyPressed(int key){
-	fprintf(stdout, "key pressed [%d]\n", key);
-    switch (key) {
-        case OF_KEY_UP:
-            bike.pedal();
-            break;
-        case OF_KEY_DOWN:
-            bike.stop();
-            break;
-        case OF_KEY_LEFT:
-            bike.handle(-0.1);
-            break;
-        case OF_KEY_RIGHT:
-            bike.handle(+0.1);
-            break;
-        case ' ':
-            bAnimate = !bAnimate;
-            break;
-        case 'a': // 原点から再開
-            bike.setLocation(ofVec2f(0, 0));
-            bike.resetHandle();
-            break;
-        case '1': // ハンドル角のみ初期化
-            bike.resetHandle();
-            break;
-        case '/': // テスト送信実行
-            bNeedSending = true;
-            cout << "OSC Sending Toggled to [" << bNeedSending << "]" << endl;
-            break;
-        default:
-            break;
-    }
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
